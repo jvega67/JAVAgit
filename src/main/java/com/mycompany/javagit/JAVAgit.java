1e4fe44b4e5e939671ffe315b5f0e9a003915b78
Juan Carlos Vega <jvega67@gmail.com>
2024-02-06 01:43:06 +0000

    Primera versión EjemploGit 3

---
+++ src/main/java/com/mycompany/javagit/JAVAgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javagit;
+
+/**
+ *
+ * @author jvega
+ */
+public class JAVAgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello github!");
+    }
+}
